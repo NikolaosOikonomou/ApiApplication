ad7384cfade90448c4b997ef57aaa765aafb23fe
git_Nikos <nikosoikonomou@gmail.com>
2022-07-09 21:11:42 +0300

    Repositry done,Controller(GetAll,Create,Delete) with View done

---
+++ ApiApplication/Controllers/ApiControllers/ProductApiController.cs
@@ -0,0 +1,70 @@
+﻿using ApiApplication.Models;
+using ApiApplication.MyApp;
+using ApiApplication.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApiApplication.Controllers.ApiControllers
+{
+    public class ProductApiController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        private ProductRepository productRepo;
+
+        public ProductApiController()
+        {
+            productRepo = new ProductRepository(db);
+        }
+       
+        public ActionResult GetAllProducts()
+        {
+            var products = productRepo.GetAllProducts();
+            return Json(products, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteProductById(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var product = productRepo.GetProductById(id);
+            if(product == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            if (ModelState.IsValid)
+            {
+                productRepo.Delete(product);
+                return Json(product, JsonRequestBehavior.AllowGet);
+            }
+           return new HttpStatusCodeResult(HttpStatusCode.NotAcceptable);
+        }
+
+        [HttpPost]
+        public ActionResult CreateProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                productRepo.Add(product);
+                return Json(product, JsonRequestBehavior.AllowGet);
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.NotAcceptable);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ApiApplication/Repositories/ProductRepository.cs
@@ -0,0 +1,56 @@
+﻿using ApiApplication.Models;
+using ApiApplication.MyApp;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ApiApplication.Repositories
+{
+    /// <summary>
+    ///All methods that interact with the database on behalf Product Model
+    /// </summary>
+    public class ProductRepository
+    {
+        private ApplicationDbContext db;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return db.Products.ToList();
+        }
+
+        public Product GetProductById(int? id)
+        {
+            return db.Products.Find(id);
+        }
+
+        public void Add(Product product)
+        {
+            db.Entry(product).State = EntityState.Added;
+            Save();
+        }
+
+        public void Edit(Product product)
+        {
+            db.Entry(product).State = EntityState.Modified;
+            Save();
+        }
+
+        public void Delete(Product product)
+        {
+            db.Entry(product).State = EntityState.Deleted;
+            Save();
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
